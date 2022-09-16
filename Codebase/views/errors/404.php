1512e811d37b1207fc9432170842c2c632263244
fanchDev <51495972+eIfo1@users.noreply.github.com>
2022-09-16 19:50:40 -0400

    updated 404 to work with view engine

--- Codebase/views/errors/404.php
+++ Codebase/views/errors/404.php
@@ -1,16 +1,41 @@
-<?php
-$name = "Not Found";
-require_once("{$_SERVER['DOCUMENT_ROOT']}/views/header.php");
-?>
-
-<h1>
-  Not Found!
-</h1>
-<p>
-  The page you requested wasn't found. Strange!
-</p>
-
-
-<?php
-require_once("{$_SERVER['DOCUMENT_ROOT']}/views/footer.php");
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>404 Not Found</title>
+  <style>
+    body {
+      background: rgb(12,12,12);
+      font-family:Montserrat, Arial, Helvetica, sans-serif;
+      color: #fff;
+    }
+    .not-found {
+      padding: 25px;
+      background-color: black;
+      display: block;
+    }
+    * {
+      box-sizing: border-box;
+    }
+    .container {
+      max-width: 400px;
+      margin: auto;
+    }
+    h1, p {
+      margin: 0;
+    }
+  </style>
+</head>
+<body>
+    <div class="container">
+      <div class="not-found">
+        <h1>
+          404 - Not Found 
+        </h1>
+        <p>We couldn't find that page on our server.</p>
+      </div>
+    </div>
+</body>
+</html>
