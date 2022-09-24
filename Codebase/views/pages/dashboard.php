b572647879606a832389f37b77e5fd19ee1459b8
fanchDev <51495972+eIfo1@users.noreply.github.com>
2022-09-24 18:49:13 -0400

    all new dashboard with cdn files and css changes

--- Codebase/views/pages/dashboard.php
+++ Codebase/views/pages/dashboard.php
@@ -3,6 +3,7 @@
 use brickspace\controller\auth\StatusController;
 use brickspace\middleware\Auth;
 use brickspace\utils\Toast;
+use brickspace\helpers\Time;
 
 Auth::Require();
 
@@ -15,6 +16,10 @@ if (@$_SESSION['note']) {
   new Toast($_SESSION['note'], 1);
   unset($_SESSION['note']);
 }
+$statement = $conn->prepare("SELECT * FROM users WHERE user_name = :username");
+$statement->bindParam(':username', $_SESSION['Username'], PDO::PARAM_STR);
+$statement->execute();
+$result = $statement->fetch();
 ?>
 
 <script>
@@ -27,17 +32,52 @@ if (@$_SESSION['note']) {
   });
 </script>
 <div class="row">
-  <div class="col-6">
-    <div class="card">
-      <h1 class="text-center">Dashboard</h1>
-      <form action="/dashboard/status/" method="POST">
-        <h3>
-          Status
-        </h3>
-        <span>
-          <?php echo StatusController::Get(); ?>
-        </span>
+  <div class="col-3">
+    <div class="ellipsis">
+      <div class="speech-bubble">
+        <?php
+        echo $result['user_status'];
+        ?>
+      </div>
+      <br>
+      <div class="card">
+        <img src="/cdn/img/no-file.png" alt="Avatar" style="  display: block;
+  margin-left: auto;
+  margin-right: auto;">
+        <h2 class="center">
+          <?php echo $result['user_name']; ?>
+        </h2>
+      </div>
+      <div class="card">
+        <h2 class="center">
+          User Info
+        </h2>
         <br>
+        <label>
+          Account Birth: <span class="small" style="float: right;"><?php echo date("l, F d, Y", strtotime($result['user_created'])) ?></span>
+        </label>
+        <br>
+        <label>
+          Friends: <span class="small" style="float: right;">Placeholder</span>
+        </label>
+        <br>
+        <label>
+          Wall Posts:
+          <span class="small" style="float: right;">
+            <?php
+            //echo WallController::PostAmount($_SESSION['UserID']); 
+            ?>
+          </span>
+        </label>
+      </div>
+    </div>
+  </div>
+  <div class="col-4">
+    <div class="card">
+      <h1 class="center">Dashboard</h1>
+    </div>
+    <div class="card">
+      <form action="/dashboard/status/" method="POST">
         <h3>
           Update Status
         </h3>
@@ -52,9 +92,9 @@ if (@$_SESSION['note']) {
       </form>
     </div>
   </div>
-  <div class="col-6">
+  <div class="col-5">
     <div class="card">
-      <h1 class="text-center">Comments</h1>
+      <h1 class="center">Website Wall</h1>
     </div>
     <div id="comments"></div>
     <div class="card">
