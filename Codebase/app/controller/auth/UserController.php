0bf833e40d02a046830dc0e33e1682503089f3ac
fanchDev <51495972+eIfo1@users.noreply.github.com>
2022-09-25 16:51:56 -0400

    fixed auth bug in currency function

--- Codebase/app/controller/auth/UserController.php
+++ Codebase/app/controller/auth/UserController.php
@@ -6,11 +6,12 @@ use brickspace\middleware\Auth;
 
 class UserController {
   public static function Currency() {
-    Auth::Require();
-    include($_SERVER['DOCUMENT_ROOT'] . "/config/config.php");
-    $currency = $conn->prepare("SELECT cubes FROM users WHERE user_id=?");
-    $currency->execute([$_SESSION['UserID']]);
-    $currency = $currency->fetch();
-    return $currency['cubes'];
+    if(Auth::Auth()) {
+      include($_SERVER['DOCUMENT_ROOT'] . "/config/config.php");
+      $currency = $conn->prepare("SELECT cubes FROM users WHERE user_id=?");
+      $currency->execute([$_SESSION['UserID']]);
+      $currency = $currency->fetch();
+      return $currency['cubes'];
+    }
   }
 }
