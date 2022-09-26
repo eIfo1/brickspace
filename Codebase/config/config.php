77711795348f5ed282709504ce53e86524e9376d
fanchDev <51495972+eIfo1@users.noreply.github.com>
2022-09-26 19:16:17 -0400

    Update config.php

--- Codebase/config/config.php
+++ Codebase/config/config.php
@@ -7,8 +7,7 @@ $whitelist = array('127.0.0.1', "::1");
 try {
   if (!in_array($_SERVER['REMOTE_ADDR'], $whitelist)) {
     // not valid
-    echo "test";
-    $conn = new PDO("mysql:doadmin:AVNS_F7v0091UCWwurZGDMPT@brickspace-db-do-user-11026856-0.b.db.ondigitalocean.com:25060/defaultdb?ssl-mode=REQUIRED");
+    $conn = new PDO('mysql:host=brickspace-db-do-user-11026856-0.b.db.ondigitalocean.com:25060;port=3309;dbname=defaultdb', 'doadmin', 'AVNS_F7v0091UCWwurZGDMPT');
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } else {
     $conn = new PDO("mysql:host=localhost;dbname=forum2", "root", "DatabasePass");
